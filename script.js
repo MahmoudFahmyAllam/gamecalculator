01458bcc3591388fc9f54b006b3451d1354176db
MahmoudFahmyAllam <126775680+MahmoudFahmyAllam@users.noreply.github.com>
2024-08-06 12:52:24 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,118 @@
+// script.js
+let display = document.getElementById('number');
+
+let currentValue = localStorage.getItem('currentValue') ? parseInt(localStorage.getItem('currentValue')) : 0;
+
+
+
+
+
+
+function changeNumber(amount) {
+    currentValue += amount;
+    updateDisplay();
+    saveToLocalStoragebc();
+}
+
+function surprise() {
+    let randomValue = Math.floor(Math.random() * 5) + 1;
+    currentValue += randomValue;
+    updateDisplay();
+    saveToLocalStoragebc();
+}
+
+function saveToLocalStoragebc() {
+    localStorage.setItem('currentValue', currentValue);
+}
+
+function updateDisplay() {
+    display.textContent = currentValue;
+}
+
+let display2 = document.getElementById('numberT');
+let currentValue2 = localStorage.getItem('currentValue2') ? parseInt(localStorage.getItem('currentValue2')) : 0;
+
+
+function changeTime(amount) {
+    currentValue2 += amount;
+    updateDisplay2();
+    saveToLocalStoragebt();
+}
+
+function surprise2() {
+    let randomValuet = Math.floor(Math.random() * 5) + 1;
+    currentValue2 += randomValuet;
+    updateDisplay2();
+    saveToLocalStoragebt();
+}
+function saveToLocalStoragebt() {
+    localStorage.setItem('currentValue2', currentValue2);
+}
+
+function updateDisplay2() {
+    display2.textContent = currentValue2;
+}
+
+// script.js
+let rdisplay = document.getElementById('rnumber');
+let rcurrentValue = localStorage.getItem('rcurrentValue') ? parseInt(localStorage.getItem('rcurrentValue')) : 0;
+
+
+
+function rchangeNumber(amount) {
+    rcurrentValue += amount;
+    rupdateDisplay();
+    saveToLocalStoragerc();
+}
+
+function rsurprise() {
+    let randomValue = Math.floor(Math.random() * 5) + 1;
+    rcurrentValue += randomValue;
+    rupdateDisplay();
+    saveToLocalStoragerc();
+}
+
+function saveToLocalStoragerc() {
+    localStorage.setItem('rcurrentValue', rcurrentValue);
+}
+
+
+function rupdateDisplay() {
+    rdisplay.textContent = rcurrentValue;
+}
+
+let rdisplay2 = document.getElementById('rnumberT');
+let rcurrentValue2 = localStorage.getItem('rcurrentValue2') ? parseInt(localStorage.getItem('rcurrentValue2')) : 0;
+
+function rchangeTime(amount) {
+    rcurrentValue2 += amount;
+    rupdateDisplay2();
+    saveToLocalStoragert();
+}
+
+function rsurprise2() {
+    let randomValuet = Math.floor(Math.random() * 5) + 1;
+    rcurrentValue2 += randomValuet;
+    rupdateDisplay2();
+    saveToLocalStoragert();
+}
+function saveToLocalStoragert() {
+    localStorage.setItem('rcurrentValue2', rcurrentValue2);
+}
+
+function rupdateDisplay2() {
+    rdisplay2.textContent = rcurrentValue2;
+}
+
+
+
+updateDisplay();
+updateDisplay2();
+rupdateDisplay();
+rupdateDisplay2();
+
+
+
+
+// script.js
+
